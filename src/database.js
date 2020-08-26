89c48c26765eb1fb1aecf3d1aec68c759406f0f2
Daniel Limas <amedlimas97@gmail.com>
2020-08-25 23:52:16 -0600

    Update database.js

--- src/database.js
+++ src/database.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
 
+const atlas = 'mongodb+srv://user1:VvkN1O2ttabd5UpY@cluster0-ezqai.mongodb.net/<dbname>?retryWrites=true&w=majority'
 const url = 'mongodb://localhost/notesapp'
-mongoose.connect(url, {
+mongoose.connect(atlas, {
     useCreateIndex: true,
     useNewUrlParser: true,
     useFindAndModify: false,
